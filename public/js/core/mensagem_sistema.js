90977a0c3167e778b5facb0c3e1fc96cb24ebc87
Rhaaby <rhaabyalves@unitins.br>
2026-01-08 20:01:34 -0300

    Delete public directory

--- public/js/core/mensagem_sistema.js
+++
@@ -1,9 +0,0 @@
-function mostrarMensagem(texto, tipo ="sucesso") {
-    const msg = document.createElement("div");
-    msg.className = `msg $ {tipo}`;
-    msg.innerText = texto;
-
-    document.body.appendChild(msg);
-
-    setTimeout(() => msg.remove(), 3000);
-}
--- public/js/user/mensagem_menu.js
+++
@@ -1,6 +0,0 @@
-const btnMenu = document.querySelector(".btn-menu");
-const gaveta = document.querySelector(".chat-gaveta");
-
-btnMenu.addEventListener("click", () => {
-  gaveta.classList.toggle("aberta");
-});
